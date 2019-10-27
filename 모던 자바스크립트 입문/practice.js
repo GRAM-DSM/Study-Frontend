3a2d149a456794a8c0bb97c81a591a05f6b60e50
yasuo3 <sungdm0303@gmail.com>
2019-10-27 23:56:46 +0900

    update 자바스크립트.md

--- 모던 자바스크립트 입문/practice.js
+++ 모던 자바스크립트 입문/practice.js
@@ -1,2 +1,5 @@
-var t = String.raw`Man errs as long as\nhe strives.`;
-console.log(t);
+var square = function sq(x) { return x * x; };
+console.log(square(3));
+var then = new Date(2008, 5, 10);
+var elapsed = new - then;
+console.log(elapsed);
