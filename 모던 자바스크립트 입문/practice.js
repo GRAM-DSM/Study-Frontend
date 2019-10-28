a9ac3c51b9b66be1d0973ac61b8b97a70de0b141
yasuo3 <sungdm0303@gmail.com>
2019-10-28 13:01:12 +0900

    update 자바스크립트.md

--- 모던 자바스크립트 입문/practice.js
+++ 모던 자바스크립트 입문/practice.js
@@ -1,5 +1,4 @@
-var square = function sq(x) { return x * x; };
-console.log(square(3));
-var then = new Date(2008, 5, 10);
-var elapsed = new - then;
-console.log(elapsed);
+var a = ["A", "B", "C"];
+a[4]="E";
+console.log(a);
+console.log(a.length);
